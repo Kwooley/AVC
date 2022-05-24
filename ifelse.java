462ab77a5da8ba196939f4ef1d3ede087c327ca6
Kyu Lee <kwooler@gmail.com>
2022-05-23 18:27:27 -0700

    first commit

---
+++ ifelse.java
@@ -0,0 +1,17 @@
+import java.sql.Time;
+import java.util.Random;
+
+public class ifelse {
+	public static void main(String[] args) {
+		Random rd = new Random(System.currentTimeMillis());
+		int num = rd.nextInt(100);
+
+		if (num % 2 == 0) {
+			System.out.println(Integer.toString(num) + " is an Even number");
+		} else {
+			System.out.println(num + " is an odd number");
+		}
+		System.out.println("Program terminated");
+	}
+
+}

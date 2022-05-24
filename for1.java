7584ec2b500e8d0f587a922a1e495f0ab66d049a
Kyu Lee <kwooler@gmail.com>
2022-05-23 19:52:22 -0700

    for

---
+++ for1.java
@@ -0,0 +1,13 @@
+public class for1 {
+	public static void main(String[] args) {
+		final int N = 10;
+		int sum = 0;
+		int i;
+		for (i = 0; i < N; i++) {
+			sum += i;
+		}
+		System.out.println("\nfor loop is termiated. The value of i is " + i);
+		System.out.println("Total: " + sum);
+	}
+
+}
--- while1.java
+++ while1.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class while1 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Boolean flag = true;
+		int num;
 
+		while (flag) {
+			num = sc.nextInt();
+			System.out.println("Input number is " + num);
+			if (num == -1)
+				flag = false;
+
+		}
+		System.out.println("While loop is terminated");
+		sc.close();
 	}
 }
